5b3861d76e104d151581c311789c6ebd0f27a575
pauldamien <75174172+pauldamien@users.noreply.github.com>
2023-12-15 16:04:17 +0000

    s1

---
+++ service-product/src/test/java/net/group/paul/PaulApplicationTests.java
@@ -0,0 +1,13 @@
+package net.group.paul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
