d76cc9ca4d89c98c89be3d0548a9d0602be66084
pauldamien <75174172+pauldamien@users.noreply.github.com>
2023-12-15 09:49:35 +0000

    intialisation
    demarrons

---
+++ lyali/src/main/java/net/group/lyali/LyaliApplication.java
@@ -0,0 +1,13 @@
+package net.group.lyali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyaliApplication.class, args);
+	}
+
+}
---
+++ lyali/src/test/java/net/group/lyali/LyaliApplicationTests.java
@@ -0,0 +1,13 @@
+package net.group.lyali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
